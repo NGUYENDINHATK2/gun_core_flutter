68176c83058d637acf568578b585f55ad7a044c2
at.nguyendinh <dinhatnguyen81@gmail.com>
2025-07-26 18:02:28 +0700

    enums

---
+++ lib/src/bloc/sync/sync_state.dart
@@ -0,0 +1,44 @@
+import 'package:gun_core_flutter/src/entity/sync.dart';
+
+abstract class SyncState {}
+
+class SyncInitialState extends SyncState {}
+
+class SyncCheckingState extends SyncState {
+  SyncCheckingState();
+}
+
+class SyncLoadingState extends SyncState {}
+
+class SyncErrorState extends SyncState {
+  final String message;
+
+  SyncErrorState(this.message);
+}
+
+class SyncUploadingState extends SyncState {
+  final SyncEntity syncEntity;
+  final Map<EMediaType, Map<String, ESyncStatus>> syncItems;
+
+  SyncUploadingState({
+    required this.syncEntity,
+    Map<EMediaType, Map<String, ESyncStatus>>? syncItems = const {},
+  }): syncItems = syncItems ?? <EMediaType, Map<String, ESyncStatus>>{};
+
+  SyncUploadingState copyWith({
+    SyncEntity? syncEntity,
+    Map<EMediaType, Map<String, ESyncStatus>>? syncItems,
+  }) {
+    return SyncUploadingState(
+      syncEntity: syncEntity ?? this.syncEntity,
+      syncItems: syncItems ?? this.syncItems,
+    );
+  }
+
+}
+
+class SyncSuccessState extends SyncState {
+  final SyncEntity syncEntity;
+
+  SyncSuccessState({required this.syncEntity});
+}
