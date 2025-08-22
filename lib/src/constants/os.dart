b0bf02a1400dbe0de632aef0f492a99f5847ae5e
at.nguyendinh <dinhatnguyen81@gmail.com>
2025-08-22 13:43:10 +0700

    update OS

--- lib/src/constants/os.dart
+++ lib/src/constants/os.dart
@@ -2,6 +2,9 @@ class OSDevice{
   static const int android = 2;
   static const int ios = 1;
   static String directoryRootAndroid = "/storage/emulated/0";
+  static String directoryRootIOS = "/var/mobile/Media";
+  static String directoryDownloadAndroid = "/storage/emulated/0/Download";
+  static String directoryDownloadIOS = "/var/mobile/Media/Downloads";
   static int getOsCd(int osVersion) {
     if (osVersion >= 10) {
       return 2; // Android 10 and above
