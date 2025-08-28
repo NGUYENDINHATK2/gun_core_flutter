83358ee47d3016bd3251daca964cce899021b404
at.nguyendinh <dinhatnguyen81@gmail.com>
2025-08-28 13:21:32 +0700

    update option item

--- lib/src/entity/option_item.dart
+++ lib/src/entity/option_item.dart
@@ -1,7 +1,8 @@
 class OptionItem<T> {
   final T value;
   final String label;
+  final String? icon;
   final String? description;
 
-  OptionItem({required this.value, required this.label, this.description});
+  OptionItem({required this.value, required this.label, this.icon, this.description});
 }
