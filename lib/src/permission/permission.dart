33494134efd298c1f7bf768b57b886c47548b979
tranlong123 <56588433+tranlong123@users.noreply.github.com>
2025-10-06 13:50:50 +0700

    add case permission (#1)
    Co-authored-by: DESKTOP-5BD4220\USER-COPRO <longtran@co-pro.vn>

--- lib/src/permission/permission.dart
+++ lib/src/permission/permission.dart
@@ -3,12 +3,20 @@ import 'package:permission_handler/permission_handler.dart';
 class PermissionResult {
   final List<Permission> denied;
   final List<Permission> permanentlyDenied;
+  final List<Permission> provisional; // *Only supported on iOS (iOS12+).*
+  final List<Permission> limited; // *Only supported on iOS (iOS14+).*
   final bool allGranted;
 
   PermissionResult({
     required this.denied,
     required this.permanentlyDenied,
-  }) : allGranted = denied.isEmpty && permanentlyDenied.isEmpty;
+    required this.limited,
+    required this.provisional,
+  }) : allGranted =
+           denied.isEmpty &&
+           permanentlyDenied.isEmpty &&
+           limited.isEmpty &&
+           provisional.isEmpty;
 }
 
 class PermissionHelper {
@@ -17,18 +25,34 @@ class PermissionHelper {
 
     final denied = <Permission>[];
     final permanentlyDenied = <Permission>[];
+    final limited = <Permission>[];
+    final provisional = <Permission>[];
 
     statuses.forEach((permission, status) {
-      if (status.isPermanentlyDenied) {
-        permanentlyDenied.add(permission);
-      } else if (!status.isGranted) {
-        denied.add(permission);
+      switch (status) {
+        case PermissionStatus.denied:
+          denied.add(permission);
+          break;
+        case PermissionStatus.limited: // iOS 14+
+          limited.add(permission);
+          break;
+        case PermissionStatus.provisional: // iOS 12+
+          provisional.add(permission);
+          break;
+        case PermissionStatus.permanentlyDenied:
+        case PermissionStatus.restricted:
+          permanentlyDenied.add(permission);
+          break;
+        default:
+          break;
       }
     });
 
     return PermissionResult(
       denied: denied,
       permanentlyDenied: permanentlyDenied,
+      limited: limited,
+      provisional: provisional,
     );
   }
 }
